2068a61391f21567e0c41a58ee6c22383a658973
drunin89 <drunin89@gmail.com>
2017-02-19 17:33:28 +0200

    Upload strings lab

---
+++ Programming with C#/2.Programming Fundamentals - January 2017/16.Strings-Lab/Strings-Lab/01.ReverseString/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ReverseString
+{
+    class ReverseString
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var result = text.Reverse();
+            Console.WriteLine(string.Join(null ,result));
+
+        }
+    }
+}
---
+++ Programming with C#/2.Programming Fundamentals - January 2017/16.Strings-Lab/Strings-Lab/03.TextFilter/TextFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.TextFilter
+{
+    class TextFilter
+    {
+        public static void Main()
+        {
+
+            string[] banWords = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries); 
+            string text = Console.ReadLine();
+            foreach (var banWord in banWords)
+            {
+                    text = text.Replace(banWord, new string('*', banWord.Length));
+            }
+            Console.WriteLine(text);
+
+        }
+    }
+}

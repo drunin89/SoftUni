867e7a17f9f8e40876f53e37ea8d61d85977afaf
drunin89 <drunin89@gmail.com>
2017-02-24 21:42:39 +0200

    Upload ExamPreparation IV

---
+++ Programming with C#/2.Programming Fundamentals - January 2017/23.ExamPreparationIV/ExamPreparationIV/01.SweetDessert/SweetDessert.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SweetDessert
+{
+    class SweetDessert
+    {
+        public static void Main()
+        {
+            var amountCash = decimal.Parse(Console.ReadLine());
+            var numberGuest = int.Parse(Console.ReadLine());
+            var priceBananas = decimal.Parse(Console.ReadLine());
+            var priceEggs = decimal.Parse(Console.ReadLine());
+            var priceBerries = decimal.Parse(Console.ReadLine());
+
+            var portions = numberGuest / 6;
+            if (numberGuest % 6 != 0)
+            {
+                portions++;
+            }
+
+            var pricePerPortion = 2 * priceBananas + 4 * priceEggs + 0.2m * priceBerries;
+            var totalPrice = pricePerPortion * portions;
+
+            var moneyLeft = amountCash - totalPrice;
+            if (moneyLeft >= 0)
+            {
+                Console.WriteLine($"Ivancho has enough money - it would cost {totalPrice:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivancho will have to withdraw money - he will need {Math.Abs(moneyLeft):F2}lv more.");
+            }
+
+        }
+    }
+}
---
+++ Programming with C#/2.Programming Fundamentals - January 2017/23.ExamPreparationIV/ExamPreparationIV/04.CubicMessages/CubicMessages.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class CubicMessages
+{
+    static void Main()
+    {
+        string encryptedMessage = Console.ReadLine();
+
+        while (!encryptedMessage.Equals("Over!"))
+        {
+            int count = int.Parse(Console.ReadLine());
+            var pattern = new Regex(@"^([0-9]+)([a-zA-Z]{" + count + @"})([^a-zA-Z]*)$");
+
+            var match = pattern.Match(encryptedMessage);
+
+            if (match.Success)
+            {
+                var message = match.Groups[2].ToString();
+                Console.Write(message + " == ");
+                var indexes = GetIndexes(match);
+
+                foreach (var i in indexes)
+                {
+                    if (0 <= i && i < message.Length)
+                    {
+                        Console.Write(message[i]);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            encryptedMessage = Console.ReadLine();
+        }
+
+    }
+
+    private static List<int> GetIndexes(Match match)
+    {
+        var indexes = new List<int>();
+        string left = match.Groups[1].ToString();
+        for (int i = 0; i < left.Length; i++)
+        {
+            if (Char.IsDigit(left[i]))
+            {
+                indexes.Add(int.Parse(left[i].ToString()));
+            }
+        }
+
+        string right = match.Groups[3].ToString();
+        for (int i = 0; i < right.Length; i++)
+        {
+            if (Char.IsDigit(right[i]))
+            {
+                indexes.Add(int.Parse(right[i].ToString()));
+            }
+        }
+
+        return indexes;
+    }
+}
